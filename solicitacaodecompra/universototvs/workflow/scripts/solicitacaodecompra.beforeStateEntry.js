8cef661b44b5ebe2f5a5c65bf056be0be6cf0e07
jeferson.francisco@fluig.com <jeferson.francisco@fluig.com>
2019-06-20 13:50:43 -0300

    universo totvs - ajuste URL

--- solicitacaodecompra/universototvs/workflow/scripts/solicitacaodecompra.beforeStateEntry.js
+++ solicitacaodecompra/universototvs/workflow/scripts/solicitacaodecompra.beforeStateEntry.js
@@ -8,16 +8,15 @@ function beforeStateEntry(sequenceId){
 	/*Etapa de cotação*/
 	if (sequenceId == 2) {
 		
-		parametros.put("MESSAGE","Nova cotação, acesse: https://wcm.fluig.com/portal/<CODE_EMPRESA>/<CODE_PAGINA_PUBLICA>/" +
+		parametros.put("MESSAGE","Nova cotação, acesse: https://universo.fluig.io/portal/fluig_thf_api_lab/supplier/" +
 				"supplier-quotation?isFullScreen=true&WKNumProces=" + getValue("WKNumProces"));
 		notifier.notify(getValue("WKUser"), "cotacao_fornecedor", parametros, destinatarios, "text/html");
 	
 	/*Etapa de Envio do pedido*/
 	} else if (sequenceId == 6) {
 		
-		parametros.put("MESSAGE","Cotação Aprovada, aguardando o envio dos produtos. Ao enviar os produtos acesse " +
-				"https://wcm.fluig.com/portal/<CODE_EMPRESA>/<CODE_PAGINA_PUBLICA>/send-order?isFullScreen=true&WKNumProces=" 
-				+ getValue("WKNumProces"));
+		parametros.put("MESSAGE","Nova cotação, acesse: https://universo.fluig.io/portal/fluig_thf_api_lab/supplier/" +
+				"supplier-quotation?isFullScreen=true&WKNumProces=" + getValue("WKNumProces")+ "&status=approved");
 		notifier.notify(getValue("WKUser"), "cotacao_fornecedor", parametros, destinatarios, "text/html");
 	}	
 }
